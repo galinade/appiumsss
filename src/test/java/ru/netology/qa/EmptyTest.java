9dc44b91ef7338944912546d55644a8890e12c74
Galina Demidova <galinagriz77@gmail.com>
2023-01-23 14:35:23 +0300

    2 test

--- src/test/java/ru/netology/qa/EmptyTest.java
+++ src/test/java/ru/netology/qa/EmptyTest.java
@@ -1,6 +1,7 @@
 package ru.netology.qa;
 
 import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.functions.ExpectedCondition;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -10,13 +11,18 @@ import io.appium.java_client.MobileElement;
 
         import java.net.MalformedURLException;
         import java.net.URL;
+import java.util.concurrent.TimeUnit;
 
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 
 public class EmptyTest {
-    private AppiumDriver driver;
+    private AndroidDriver driver;
 
     @BeforeEach
     public void setUp() throws MalformedURLException {
@@ -35,21 +41,37 @@ public class EmptyTest {
         driver = new AndroidDriver(remoteUrl, desiredCapabilities);
     }
 
+
     @Test
     public void sampleTest() {
-        MobileElement el1 = (MobileElement) driver.findElementById("ru.netology.testing.uiautomator:id/userInput");
+        MobileElement el1 = (MobileElement) driver.findElementById("ru.netology.testing.uiautomator:id/textToBeChanged");
         el1.click();
-        MobileElement el2 = (MobileElement) driver.findElementById("ru.netology.testing.uiautomator:id/buttonChange");
+        MobileElement el2 = (MobileElement) driver.findElementById("ru.netology.testing.uiautomator:id/userInput");
+        el2.sendKeys("   ");
         el2.click();
-        MobileElement el3 = (MobileElement) driver.findElementById("ru.netology.testing.uiautomator:id/textToBeChanged");
+        MobileElement el3 = (MobileElement) driver.findElementById("ru.netology.testing.uiautomator:id/buttonChange");
         el3.click();
         MobileElement el4 = (MobileElement) driver.findElementById("ru.netology.testing.uiautomator:id/textToBeChanged");
-        el4.click();
-        MobileElement el5 = (MobileElement) driver.findElementById("ru.netology.testing.uiautomator:id/textToBeChanged");
-        el5.isDisplayed();
-        Assertions.assertEquals(el5.getText(),"Hello UiAutomator!");
-
+        el4.isDisplayed();
+        Assertions.assertEquals(el4.getText(),el1.getText());
     }
+    @Test
+    public void sampleTest1() {
+        MobileElement el1 = (MobileElement) driver.findElementById("ru.netology.testing.uiautomator:id/userInput");
+        el1.sendKeys("12345");
+        el1.click();
+
+        MobileElement el2 = (MobileElement) driver.findElementById("ru.netology.testing.uiautomator:id/buttonActivity");
+        el2.click();
+        WebDriverWait wait = new WebDriverWait(driver,10);
+        wait.until(ExpectedConditions.invisibilityOfElementLocated(By.cssSelector("ru.netology.testing.uiautomator:id/text")));
+
+        MobileElement el3 = (MobileElement) driver.findElementById("ru.netology.testing.uiautomator:id/text");
+        el3.isDisplayed();
+        Assertions.assertEquals(el3.getText(),"12345");
+    }
+
+  
 
     @AfterEach
     public void tearDown() {
